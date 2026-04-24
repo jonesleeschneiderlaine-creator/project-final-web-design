30b9701da26fd26a0caf5b2d11546163b5b33db2
samm1735 <ramclief17@gmail.com>
2026-04-24 09:34:15 -0400

    Fix: Added page not found and made routing cleaner

--- project-final/src/components/plateforme/PlateformeLayout.jsx
+++ project-final/src/components/plateforme/PlateformeLayout.jsx
@@ -1,10 +1,13 @@
-
+import { Outlet } from "react-router-dom"
 import "./plateformeLayout.css"
 
 const PlateformeLayout = () => {
   return (
     <div>
         <h1>Plateforme Layout</h1>
+        <main>
+          <Outlet />
+        </main>
     </div>
   )
 }
---
+++ project-final/src/components/plateforme/Sidebar/Sidebar.jsx
@@ -0,0 +1,11 @@
+import "./sidebar.css"
+
+const Sidebar = () => {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default Sidebar
